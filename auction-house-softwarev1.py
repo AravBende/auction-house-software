68923966893f79780279ba46a65687d1275d99bb
Arav Bende <92068151+AravBende@users.noreply.github.com>
2022-11-27 18:18:15 +0800

    Update auction-house-softwarev1.py

--- auction-house-softwarev1.py
+++ auction-house-softwarev1.py
@@ -1,6 +1,7 @@
-auction_items = [[6926, "Mono Lisa", 1030312, 0], [1828, "girl with pearl earring", 5012300, 0], [9598, "the starry night", 7480120, 0], [1553, "saint john the baptist", 6038467, 0], [4808, "The last supper", 723404, 0],
-                 [4799, "La Vierge aux rochers", 2384782, 0], [9657, "The Vitruvian Man", 2783480, 0], [8877, "leanardo davinci self portrait", 8923748, 0], [1544, "the screaming man", 9237493, 0], [7416, "salvator mundi", 450000000, 0]]
-
+auction_items = [[6926, "Mono Lisa", 1030312, 0], [1828, "girl with pearl earring", 5012300, 0], [9598, "the starry night", 7480120, 0],
+                 [1553, "saint john the baptist", 6038467, 0], [4808, "The last supper", 723404, 0],
+                 [4799, "La Vierge aux rochers", 2384782, 0], [9657, "The Vitruvian Man", 2783480, 0],
+                 [8877, "leanardo davinci self portrait", 8923748, 0], [1544, "the screaming man", 9237493, 0], [7416, "salvator mundi", 450000000, 0]]
 
 contsearching = True
 
@@ -18,10 +19,10 @@ while contsearching == True:
             print()
             ifbid = input("would you like to bid? (y/n) ")
             if ifbid.lower() == "y":
-                item[3] += 1
                 bidamount = int(input("Please enter bid amount, must be more then " + str(item[2]) + ": "))
-                if bidamount > item[3]:
-                    item[3] = bidamount
+                if bidamount > item[2]:
+                    item[2] = bidamount
+                    item[3] += 1
                 else:
                     print("thats not enough money")
     asktocontinue = input("would you like to continue searching? (y/n) ")
